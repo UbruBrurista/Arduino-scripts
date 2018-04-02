d5527017fb7105815b569b9a5d80701188538b02
Woo <zwoo18@bu.edu>
2018-04-02 17:39:15 -0400

    add to full cycle script

--- Demo/Demo.ino
+++ Demo/Demo.ino
@@ -1,108 +1,254 @@
 #include <SoftwareSerial.h>
 
+int wait_delay = 3000;
+int last_interrupt_time = 0;
+
 // Serial variables
-SoftwareSerial mySerial(6, 7); // RX, TX
+SoftwareSerial mySerial(8, 9); // RX, TX
 int lastByte = 0;
 
 int COMMAND_FULL_CYCLE = 1;
 int COMMAND_GO_HOME = 2;
 int COMMAND_GO_WORK = 3;
+int COMMAND_GRIND = 4;
+int COMMAND_PUMP = 5;
 
 // Brew Unit Variables
-int enable_pin = 3;
-int c_motor_pin = 4;
-int d_motor_pin = 5;
+int enable_pin = 10;
+int c_motor_pin = 11;
+int d_motor_pin = 12;
 
 // Interrupt Variables
 int interrupt_pin = 2;
 int debounce_delay = 100;
 int count = 0;
 
+// Other pins
+int grinderPin = 6;
+int pumpEnable = 5;
+int flowPin = 0;
+int hallPin = 1;
+int flowCount = 0;
+int grinderCount = 0;
+int disablePin = 3;
+int waterLevelPin = 13;
+
 //state variables
 int WAIT_FOR_READ = 0;
-int GO_WORK = 1;
-int GO_HOME = 2;
+int GRIND = 1;
+int GO_WORK = 2;
+int PUMP = 3;
+int GO_HOME = 4;
+int BU_WORK = 5;
+int BU_HOME = 6;
+
+int grinderLimit = 150;
+int flowLimit = 200;
 
 int state = WAIT_FOR_READ;
 int next_state = -1;
+int bu_state = BU_HOME;
+unsigned long noInterruptUntil = 0;
 
 // Control functions
-void goToHome() {
-  digitalWrite(c_motor_pin, LOW);
-  digitalWrite(d_motor_pin, HIGH);
-  digitalWrite(enable_pin, HIGH);
+void runGrinder() {
+  Serial.print("State is: ");
+  Serial.println(state);
+  Serial.println("grinding");
+  grinderCount = 0;
+  state = GRIND;
+  next_state = GO_WORK;
+  digitalWrite(grinderPin, HIGH);
+}
+
+void grinderChange() {
+  Serial.print("State is: ");
+  Serial.println(state);
+  grinderCount++;
+  Serial.print("Grinder count: ");
+  Serial.println(grinderCount);
+
+  if (grinderCount >= grinderLimit) {
+    digitalWrite(grinderPin, LOW);
+  
+    //if (state == WAIT_FOR_READ) {
+    //  return;
+    //}
+    
+    //if (state == GRIND) {
+      for (int i =0; i<7000; i++) {
+        Serial.println(i);
+      }
+      state = GO_WORK;
+      next_state = PUMP;
+      //delay(3000);
+      goToWork();
+    //}
+  }
 }
 
 void goToWork() {
+  state = GO_WORK;
+  next_state = PUMP;
+  delay(2000);
+  Serial.print("goToWork: state is: ");
+  Serial.println(state);
+  Serial.println("going to work");
+  //noInterruptUntil = millis() + 1000;
   digitalWrite(c_motor_pin, HIGH);
   digitalWrite(d_motor_pin, LOW);
   digitalWrite(enable_pin, HIGH);
 }
 
+void goToWork_Manual() {
+  digitalWrite(c_motor_pin, HIGH);
+  digitalWrite(d_motor_pin, LOW);
+  digitalWrite(enable_pin, HIGH);
+}
+
+void disableMotorAfterOneCycle() { // added debouncing code since we're bypassing the Schmidt Trigger
+  disableMotor();
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > wait_delay) {
+    last_interrupt_time = interrupt_time;
+    if (state == GO_WORK) {
+      if (next_state == PUMP) {
+        bu_state = BU_WORK;
+        //disableAll();
+        for (int j =0; j<7000; j++) {
+          Serial.println(j);
+        }
+        state = PUMP;
+        next_state = GO_HOME;
+        runPump();
+      }
+      
+    } else if (state == GO_HOME) {
+      if (next_state == WAIT_FOR_READ) {
+        bu_state = BU_HOME;
+        //disableAll();
+        state = WAIT_FOR_READ;
+        next_state = -1;
+      }
+    }
+  }
+}
+
+void runPump() {
+  state = PUMP;
+  next_state = GO_HOME;
+  delay(3000);
+  Serial.print("runPump: state is: ");
+  Serial.println(state);
+  Serial.println("pumping");
+  flowCount = 0;
+  digitalWrite(pumpEnable, HIGH);
+}
+
+void flowChange() {
+  Serial.print("State is: ");
+  Serial.println(state);
+  flowCount++;
+  Serial.print("Flow count: ");
+  Serial.println(flowCount);
+
+  if (flowCount >= flowLimit) {
+    digitalWrite(pumpEnable, LOW);
+    //if (state == WAIT_FOR_READ) {
+    //  return;
+    //}
+    
+    //if (next_state == GO_HOME) {
+      //delay(3000);
+      for (int k =0; k<10000; k++) {
+        Serial.println(k);
+      }
+      state = GO_HOME;
+      next_state = WAIT_FOR_READ;
+      goToHome();
+    //}
+  }
+}
+
+void goToHome() {
+  state = GO_HOME;
+  next_state = WAIT_FOR_READ;
+  delay(2000);
+  Serial.print("goToHome: state is: ");
+  Serial.println(state);
+  Serial.println("going home");
+  //noInterruptUntil = millis() + 1000;
+  digitalWrite(c_motor_pin, LOW);
+  digitalWrite(d_motor_pin, HIGH);
+  digitalWrite(enable_pin, HIGH);
+}
+
+void goToHome_Manual() {
+  digitalWrite(c_motor_pin, LOW);
+  digitalWrite(d_motor_pin, HIGH);
+  digitalWrite(enable_pin, HIGH);
+}
+
 void disableMotor() {
+  Serial.println("disabling motor");
   digitalWrite(enable_pin, LOW);
   digitalWrite(c_motor_pin, LOW);
   digitalWrite(d_motor_pin, LOW);
 }
 
-void disableMotorAfterOneCycle() { // added debouncing code since we're bypassing the Schmidt Trigger
-  static unsigned long last_interrupt_time = 0;
-  unsigned long interrupt_time = millis();
-  if (interrupt_time - last_interrupt_time > debounce_delay) {
-    int next_next_state = -1;
-
-    if (state == GO_WORK) {
-      if (next_state == GO_HOME) {
-        goToWork();
-        next_next_state = WAIT_FOR_READ;
-      } else if (next_state == WAIT_FOR_READ) {
-        disableMotor();
-      }
-
-    } else if (state == GO_HOME) {
-      if (next_state == WAIT_FOR_READ) {
-        disableMotor();
-      }
-    }
-
-    state = next_state;
-    next_state = next_next_state;
-  }
-
-  last_interrupt_time = interrupt_time;
+void disableAll() {
+  Serial.println("disabling all");
+  digitalWrite(enable_pin, LOW);
+  digitalWrite(c_motor_pin, LOW);
+  digitalWrite(d_motor_pin, LOW);
+  digitalWrite(grinderPin, LOW);
+  digitalWrite(pumpEnable, LOW);
 }
 
 void interpretByte(int lastByte) {
+  Serial.print("Interpreting byte: ");
+  Serial.println(lastByte);
   if (lastByte == COMMAND_FULL_CYCLE) {
-    state = GO_WORK;
-    /**********************************
-    // Change the next line to something like `next_state = START)PUMP;` when we need to add that 
-    // (you'll need to define START_PUMP too)
-    **********************************/
-    next_state = GO_HOME;
-
-    goToWork();
+    state = GRIND;
+    next_state = GO_WORK;
+    runGrinder();
 
   } else if (lastByte == COMMAND_GO_WORK) {
-    state = GO_WORK;
-    next_state = WAIT_FOR_READ;
-    goToWork();
+    //state = GO_WORK;
+    //next_state = WAIT_FOR_READ;
+    goToWork_Manual();
 
   } else if (lastByte == COMMAND_GO_HOME) {
-    state = GO_HOME;
-    next_state = WAIT_FOR_READ;
-    goToHome();
+    //state = GO_HOME;
+    //next_state = WAIT_FOR_READ;
+    goToHome_Manual();
+    
+  } else if (lastByte == COMMAND_GRIND) {
+    //state = GRIND;
+    //next_state = WAIT_FOR_READ;
+    runGrinder();
+    
+  } else if (lastByte == COMMAND_PUMP) {
+    //state = PUMP;
+    //next_state = WAIT_FOR_READ;
+    runPump();
   }
 }
 
 void setup() {
-  pinMode(enable, OUTPUT);
+  pinMode(enable_pin, OUTPUT);
   pinMode(c_motor_pin, OUTPUT);
   pinMode(d_motor_pin, OUTPUT);
+  pinMode(grinderPin, OUTPUT);
+  pinMode(pumpEnable, OUTPUT);
   pinMode(interrupt_pin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(2), disableMotorAfterOneCycle, FALLING);
+  pinMode(waterLevelPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(interrupt_pin), disableMotorAfterOneCycle, FALLING);
+  attachInterrupt(digitalPinToInterrupt(hallPin), grinderChange, FALLING);
+  attachInterrupt(digitalPinToInterrupt(flowPin), flowChange, FALLING);
+  attachInterrupt(digitalPinToInterrupt(disablePin), disableAll, FALLING);
 
-  disableMotor();
+  disableAll();
   
   Serial.begin(4800);
   while (!Serial) {
@@ -110,29 +256,73 @@ void setup() {
   }
 
   mySerial.begin(9600);
+  Serial.println(mySerial.available());
 
   Serial.println("Setup Complete!");
 }
 
 void loop() { // run over and over
+  
+  if(digitalRead(waterLevelPin) == LOW) {
+    //Serial.println("You have enough water");
+  }
+  
+  if (digitalRead(waterLevelPin) == HIGH) {
+    disableAll();
+    state = WAIT_FOR_READ;
+    next_state = WAIT_FOR_READ;
+  }
+  
   if (state == WAIT_FOR_READ && mySerial.available()) {
-    Serial.println("reading");
     int incomingByte = mySerial.read();
-    if (incomingByte == '\n') {
-      interpretByte(lastByte);
-    } else {
-      lastByte = incomingByte;
-    }
     Serial.print("Read byte is: ");
     Serial.println(incomingByte);
+    interpretByte(incomingByte);
+  } 
+  /*else if (state == PUMP) {
+    state = PUMP;
+    delay(3000);
+    runPump();
   }
+  */
   /************************
   Add something like this when you get around to adding the pump or something: 
-    else if (state == START_PUMP) {
+    else if (state == PUMP) {
       state = PUMP_IN_PROGRESS; // you'll have to define these variables and startPump;
       next_state = START_BOILER:
       startPump();
     }
   ************************/
+
+  if (mySerial.available() && mySerial.read() == 100) {
+    disableAll();
+    state = WAIT_FOR_READ;
+  }
+  
+  
+  /*
+  switch (state) {
+    case 1:
+      delay(2000);
+      runGrinder();
+      break;
+    case 2:
+      delay(2000);
+      goToWork();
+      break;
+    case 3:
+      delay(2000);
+      goToHome();
+      break;
+    case 4:
+      delay(2000);
+      runPump();
+      break;
+    case 0:
+      break;
+    default:
+      break; 
+  }
+  */
 
 }
--- Full_Cycle/Full_Cycle.ino
+++ Full_Cycle/Full_Cycle.ino
@@ -1,5 +1,5 @@
 //initialization state variables
-bool stCycleBrew = false;
+bool stRunning = false;
 bool stGrind = false;
 bool stPrepareToBrew = false;
 bool stBrew = false;
@@ -32,7 +32,8 @@ int water; // coming from RPi (amount of water you want)
 
 //functions
 start() {
-  stCycleBrew = true;
+  stRunning = true;
+  stGrind = true;
 }
 
 
@@ -59,73 +60,75 @@ void setup() {
 
 void loop() { // aka "start"
   // put your main code here, to run repeatedly:
-  if (stGrind) {
-    if (grinderState == LOW) {
-      grinderImp = 0;
-      digitalWrite(grinderState, HIGH);
-    }
-    else {
-      if (analogRead(grinderImp) >= coffeeImp) {
-        digitalWrite(grinderState, LOW);
-        stGrind = false;
-        stPrepareToBrew = true;
-        heaterPreheated = false;
-      }
-    }
-  }
-  else if (stPrepareToBrew) {
-    if (!brewMoving && brewAtHome) {
-      goToWork();
-      brewMoving = true;
-    }
-    else {
-      if (heaterState == LOW && !heaterPreheated) {
-        digitalWrite(heaterState, HIGH);
-        // figure this part out later
+  if (stRunning) {
+    if (stGrind) {
+      if (grinderState == LOW) {
+        //grinderImp = 0;
+        digitalWrite(grinderState, HIGH);
       }
       else {
-        if (digitalRead(temp) >= tempPreheat) {
-          digitalWrite(heaterState, LOW);
-          heaterPreheated = true;
-          if (!brewAtHome && heaterPreheated) {
-            stPrepareToBrew = false;
-            stBrew = true;
+        if (analogRead(grinderImp) >= coffeeImp) {
+          digitalWrite(grinderState, LOW);
+          stGrind = false;
+          stPrepareToBrew = true;
+          heaterPreheated = false;
+        }
+      }
+    }
+    else if (stPrepareToBrew) {
+      if (!brewMoving && brewAtHome) {
+        goToWork();
+        brewMoving = true;
+      }
+      else {
+        if (heaterState == LOW && !heaterPreheated) {
+          digitalWrite(heaterState, HIGH);
+          // figure this part out later
+        }
+        else {
+          if (digitalRead(temp) >= tempPreheat) {
+            digitalWrite(heaterState, LOW);
+            heaterPreheated = true;
+            if (!brewAtHome && heaterPreheated) {
+              stPrepareToBrew = false;
+              stBrew = true;
+            }
           }
         }
       }
     }
-  }
-  else if (stBrew) {
-    if (pumpState == LOW) {
-      digitalWrite(pumpState, HIGH);
-      digitalWrite(valveOpen, LOW);
-      digitalWrite(heaterState, HIGH);
-    }
-    else {
-      if (digitalRead(waterImp) >= water) {
-        digitalWrite(pumpState, LOW);
-        digitalWrite(heaterState, LOW);
-        stBrew = false;
-        stDispose = true;
+    else if (stBrew) {
+      if (pumpState == LOW) {
+        digitalWrite(pumpState, HIGH);
+        digitalWrite(valveOpen, LOW);
+        digitalWrite(heaterState, HIGH);
+      }
+      else {
+        if (digitalRead(waterImp) >= water) {
+          digitalWrite(pumpState, LOW);
+          digitalWrite(heaterState, LOW);
+          stBrew = false;
+          stDispose = true;
+        }
       }
     }
-  }
-  else if (stDispose) {
-    if (!brewMoving && !brewAtHome) {
-      goHome();
-      brewMoving = true;
+    else if (stDispose) {
+      if (!brewMoving && !brewAtHome) {
+        goHome();
+        brewMoving = true;
+      }
+      else {
+        if (brewAtHome()) {
+          stDispose = false;
+          stRunning = false;
+        }f
+      }
     }
     else {
-      if (brewAtHome()) {
-        stDispose = false;
-        stCycleBrew = false;
-      }f
+      temp = getTemp();
+      pidOut = pidCalculate();
+      updateTimer(pidOut);
     }
-  }
-  else {
-    temp = getTemp();
-    pidOut = pidCalculate();
-    updateTimer(pidOut);
   }
 }
 
